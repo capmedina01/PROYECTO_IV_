22cd4ff22ef9afa9c0afec4e139dff6c1ec4d2bf
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-08-20 23:37:14 -0500

    metodos crud categoria

---
+++ PetShopAPI/src/main/java/com/PetShopAPI/persistence/CategoriaRepository.java
@@ -0,0 +1,46 @@
+package com.PetShopAPI.persistence;
+
+import com.PetShopAPI.persistence.crud.CategoriaCrudRepository;
+import com.PetShopAPI.persistence.entity.Categoria;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CategoriaRepository {
+
+    private CategoriaCrudRepository categoriaCrudRepository;
+
+    public Categoria createCategoria(Categoria categoria){
+        try {
+            Categoria saved = categoriaCrudRepository.save(categoria);
+            return saved;
+        }catch (Exception e){
+            throw new RuntimeException("Error al crear la categoria"+e.getMessage());
+        }
+    }
+
+    public List<Categoria> getCategorias() {
+        try {
+            return (List<Categoria>) categoriaCrudRepository.findAll();
+        }catch (Exception e){
+            throw new RuntimeException("Error al consultar las categorias"+e.getMessage());
+        }
+    }
+
+    public Categoria UpdateCategoria(Categoria categoria){
+        try {
+            Optional<Categoria> catExist = categoriaCrudRepository.findById(categoria.getCategoriaId());
+            if(catExist.isPresent()){
+                Categoria cat = catExist.get();
+                cat.setNombre(categoria.getNombre());
+                cat.setDescripcion(categoria.getDescripcion());
+
+                return categoriaCrudRepository.save(cat);
+            }
+        }catch (Exception e){
+            throw new RuntimeException("Error al actualizar la categoria"+e.getMessage());
+        }
+        return null;
+    }
+
+}
