face22379e7702a1c9aa71c0d4a103a96d382bb0
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-08-24 16:14:00 -0500

    metodos +++

--- PetShopAPI/src/main/java/com/PetShopAPI/persistence/DetalleVentaRepository.java
+++ PetShopAPI/src/main/java/com/PetShopAPI/persistence/DetalleVentaRepository.java
@@ -1,4 +1,65 @@
 package com.PetShopAPI.persistence;
 
+import com.PetShopAPI.persistence.crud.DetalleVentaCrudRepository;
+import com.PetShopAPI.persistence.entity.DetalleVenta;
+
+import java.util.List;
+import java.util.Optional;
+
 public class DetalleVentaRepository {
+
+    private DetalleVentaCrudRepository detalleVentaCrudRepository;
+
+    public DetalleVenta create(DetalleVenta detalleVenta) {
+
+        try {
+            return detalleVentaCrudRepository.save(detalleVenta);
+        }catch(Exception e) {
+            throw new RuntimeException("Error al crear el detalle de la venta"+e.getMessage());
+        }
+    }
+
+    public List<DetalleVenta> findAll() {
+
+        try {
+            return (List<DetalleVenta>) detalleVentaCrudRepository.findAll();
+        }catch(Exception e) {
+            throw new RuntimeException("Error al consultar los detalle de la venta"+e.getMessage());
+        }
+
+    }
+
+    public DetalleVenta updateDetalleVenta(DetalleVenta detalleVenta) {
+
+        try {
+            Optional<DetalleVenta> exist = detalleVentaCrudRepository.findById(detalleVenta.getDetalleId());
+            if(exist.isPresent()) {
+                DetalleVenta detalle = exist.get();
+
+                detalle.setVentaId(detalleVenta.getVentaId());
+                detalle.setCantidad(detalleVenta.getCantidad());
+                detalle.setProductoId(detalleVenta.getProductoId());
+                detalle.setPrecioUnitario(detalleVenta.getPrecioUnitario());
+                detalle.setSubTotal(detalleVenta.getSubTotal());
+                return detalleVentaCrudRepository.save(detalle);
+            }
+        }catch(Exception e) {
+            throw new RuntimeException("Error al actualizar el detalle de la venta"+e.getMessage());
+        }
+        return null;
+    }
+
+    public void deleteDetalleVenta(DetalleVenta detalleVenta) {
+
+        try{
+            Optional<DetalleVenta> exist = detalleVentaCrudRepository.findById(detalleVenta.getDetalleId());
+            if(exist.isPresent()) {
+                DetalleVenta detalle = exist.get();
+                detalleVentaCrudRepository.delete(detalle);
+            }
+
+        }catch(Exception e) {
+            throw new RuntimeException("Error al eliminar el detalle de la venta"+e.getMessage());
+        }
+    }
 }
--- PetShopAPI/src/main/java/com/PetShopAPI/persistence/MascotaRepository.java
+++ PetShopAPI/src/main/java/com/PetShopAPI/persistence/MascotaRepository.java
@@ -1,4 +1,51 @@
 package com.PetShopAPI.persistence;
 
+import com.PetShopAPI.persistence.crud.MascotaCrudRepository;
+import com.PetShopAPI.persistence.entity.Mascota;
+
+import java.util.List;
+import java.util.Optional;
+
 public class MascotaRepository {
+
+    private MascotaCrudRepository mascotaCrudRepository;
+
+    public Mascota createMascota(Mascota mascota) {
+
+        try{
+            return mascotaCrudRepository.save(mascota);
+        }catch(Exception e){
+            throw new RuntimeException("Error al crear la mascota"+e.getMessage());
+
+        }
+    }
+
+    public List<Mascota> getMascotaAll(){
+
+        try{
+            return (List<Mascota>) mascotaCrudRepository.findAll();
+        }catch(Exception e){
+            throw new RuntimeException("Error al obtener las mascotas"+e.getMessage());
+        }
+    }
+
+    public Mascota updateMascota(Mascota mascota) {
+
+        try{
+            Optional<Mascota> exist = mascotaCrudRepository.findById(mascota.getMascotaId());
+            if(exist.isPresent()){
+                Mascota mascotaActual = exist.get();
+                mascotaActual.setNombre(mascota.getNombre());
+                mascotaActual.setClienteId(mascota.getClienteId());
+                mascotaActual.setEdad(mascota.getEdad());
+                mascotaActual.setRaza(mascota.getRaza());
+                mascotaActual.setEspecie(mascota.getEspecie());
+                return mascotaCrudRepository.save(mascotaActual);
+            }
+        }catch(Exception e){
+            throw new RuntimeException("Error al actualizar la mascota"+e.getMessage());
+        }
+    }
+
+
 }
