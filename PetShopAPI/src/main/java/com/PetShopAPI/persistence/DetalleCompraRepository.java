cc58902bc140dd3afd03dd1426f0567baa1ba658
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-08-24 11:00:09 -0500

    metodos ++

--- PetShopAPI/src/main/java/com/PetShopAPI/persistence/DetalleCompraRepository.java
+++ PetShopAPI/src/main/java/com/PetShopAPI/persistence/DetalleCompraRepository.java
@@ -4,6 +4,9 @@ import com.PetShopAPI.persistence.crud.DetalleCrompaCrudRepository;
 import com.PetShopAPI.persistence.entity.DetalleCompra;
 import jakarta.persistence.PersistenceException;
 
+import java.util.List;
+import java.util.Optional;
+
 public class DetalleCompraRepository {
 
     private DetalleCrompaCrudRepository detalleCrompaCrudRepository;
@@ -14,6 +17,47 @@ public class DetalleCompraRepository {
             return detalleCrompaCrudRepository.save(detalleCompra);
         }catch (Exception e){
             throw new PersistenceException("Error al crear el detalle de compra"+e.getMessage());
+        }
+    }
+
+    public List<DetalleCompra> findAll(){
+
+        try{
+            return (List<DetalleCompra>) detalleCrompaCrudRepository.findAll();
+        }catch (Exception e){
+            throw new PersistenceException("Error al consultar los detalle de compra"+e.getMessage());
+        }
+    }
+
+    public DetalleCompra updateDetalleCompra (DetalleCompra detalleCompra) {
+
+        try {
+            Optional<DetalleCompra> exist = detalleCrompaCrudRepository.findById(detalleCompra.getDetalleId());
+            if (exist.isPresent()) {
+                DetalleCompra detalle = exist.get();
+                detalle.setDetalleId(detalleCompra.getDetalleId());
+                detalle.setCompraId(detalleCompra.getCompraId());
+                detalle.setCantidad(detalleCompra.getCantidad());
+                detalle.setPrecioUnitario(detalleCompra.getPrecioUnitario());
+                detalle.setProductoId(detalleCompra.getProductoId());
+                return detalleCrompaCrudRepository.save(detalle);
+            }
+        }catch (Exception e){
+            throw new PersistenceException("Error al actualizar el detalle de compra"+e.getMessage());
+        }
+        return null;
+    }
+
+    public void deleteDetalleCompra (DetalleCompra detalleCompra) {
+
+        try {
+            Optional<DetalleCompra> exist = detalleCrompaCrudRepository.findById(detalleCompra.getDetalleId());
+            if (exist.isPresent()) {
+                DetalleCompra detalle = exist.get();
+                detalleCrompaCrudRepository.delete(detalle);
+            }
+        }catch (Exception e){
+            throw new PersistenceException("Error al eliminar el detalle de compra"+e.getMessage());
         }
     }
 }
--- PetShopAPI/src/main/java/com/PetShopAPI/persistence/ProductoRepository.java
+++ PetShopAPI/src/main/java/com/PetShopAPI/persistence/ProductoRepository.java
@@ -1,4 +1,60 @@
 package com.PetShopAPI.persistence;
 
+import com.PetShopAPI.persistence.crud.ProductoCrudRepository;
+import com.PetShopAPI.persistence.entity.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
 public class ProductoRepository {
+
+    private ProductoCrudRepository productoCrudRepository;
+
+    public Producto createProducto(Producto producto) {
+
+        try {
+            return productoCrudRepository.save(producto);
+        }catch (Exception e) {
+            throw new RuntimeException("Error al crear el producto"+e.getMessage());
+        }
+    }
+
+    public List<Producto> findAll() {
+
+        try {
+            return (List<Producto>) productoCrudRepository.findAll();
+        }catch (Exception e) {
+            throw new RuntimeException("Error al consultar los productos"+e.getMessage());
+        }
+    }
+
+    public Producto updateProducto(Producto producto) {
+
+        try{
+            Optional<Producto> exist = productoCrudRepository.findById(producto.getProductoId());
+
+            if(exist.isPresent()) {
+                Producto existProducto = exist.get();
+                existProducto.setNombre(producto.getNombre());
+                existProducto.setDescripcion(producto.getDescripcion());
+                existProducto.setPrecio(producto.getPrecio());
+                return productoCrudRepository.save(existProducto);
+            }
+        }catch (Exception e) {
+            throw new RuntimeException("Error al actualizar el producto"+e.getMessage());
+        }
+        return null;
+    }
+
+    public void deleteProducto(Producto producto) {
+
+        try{
+            Optional<Producto> exist = productoCrudRepository.findById(producto.getProductoId());
+            if(exist.isPresent()) {
+                productoCrudRepository.delete(exist.get());
+            }
+        }catch (Exception e) {
+            throw new RuntimeException("Error al eliminar el producto"+e.getMessage());
+        }
+    }
 }
