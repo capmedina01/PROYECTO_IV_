4bd8dd6775959d5e69194792b38b2439219a05b0
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-09-10 15:29:23 -0500

    Maperstruct en category y product

--- PetShopAPI/src/main/java/com/PetShopAPI/persistence/mapper/CategoryMapper.java
+++ PetShopAPI/src/main/java/com/PetShopAPI/persistence/mapper/CategoryMapper.java
@@ -1,7 +1,23 @@
 package com.PetShopAPI.persistence.mapper;
 
+import com.PetShopAPI.domain.Category;
+import com.PetShopAPI.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
+
+    @Mappings({
+            @Mapping(source = "categoriaId", target = "categoryId"),
+            @Mapping(source = "nombre", target = "categoryName"),
+            @Mapping(source = "descripcion", target = "categoryDescription"),
+    })
+
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration
+    Categoria toCategoria(Category category);
 }
---
+++ PetShopAPI/src/main/java/com/PetShopAPI/persistence/mapper/ProductMapper.java
@@ -0,0 +1,28 @@
+package com.PetShopAPI.persistence.mapper;
+
+import com.PetShopAPI.domain.Product;
+import com.PetShopAPI.persistence.entity.Producto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    @Mappings({
+            @Mapping(source = "productoId", target = "productId"),
+            @Mapping(source = "nombre", target = "name"),
+            @Mapping(source = "descripcion", target = "description"),
+            @Mapping(source = "precio", target = "price"),
+            @Mapping(source = "stock", target = "stock"),
+            @Mapping(source = "categoriaId", target = "categoryId")
+
+    })
+    Product toProduct(Producto producto);
+
+    @InheritInverseConfiguration
+    Producto toProduct(Product product);
+}
